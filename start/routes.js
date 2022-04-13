075aa7c405f9a414bac0e8c26f240cc4089079b7
anniasebold <anniasebold3.0@gmail.com>
2022-04-13 19:30:18 -0300

    add upload images

--- start/routes.js
+++ start/routes.js
@@ -19,6 +19,9 @@ const Route = use('Route')
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
 
+Route.post('properties/:id/images', 'ImageController.store')
+  .middleware('auth')
+
 Route.resource('properties', 'PropertyController')
   .apiOnly()
   .middleware('auth')
